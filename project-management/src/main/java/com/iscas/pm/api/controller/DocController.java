2bfdbee756483cc57d8932bae6f7937206f8c7cf
lichang <li664101181@163.com>
2022-08-08 15:32:26 +0800

    项目文档接口测试+调整完毕

--- project-management/src/main/java/com/iscas/pm/api/controller/DocController.java
+++ project-management/src/main/java/com/iscas/pm/api/controller/DocController.java
@@ -236,7 +236,6 @@ public class DocController {
            if ( !reviseRecordService.remove(new QueryWrapper<ReviseRecord>().eq("id", reviseRecordId))){
                throw new IllegalArgumentException("要删除的修订记录不存在");
            }   return true;
-
     }
 
     @PostMapping("/deleteTemplate")//没添加权限
